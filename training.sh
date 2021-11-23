de94e199e6aedcfb798f272a09d50c1ad5389bd3
Carol <cjuneau@login001.cm.cluster>
2021-11-23 13:34:15 -0500

    edited scripts

--- training.sh
+++ training.sh
@@ -2,10 +2,14 @@
 #SBATCH --job-name=midi-proj
 #SBATCH -N 1
 #SBATCH -n 14
-#SBATCH --grep=gpu:1
+#SBATCH --gres=gpu:1
 #SBATCH --output job%j.out
 #SBATCH --error job%j.err
 #SBATCH -p gpu-v100-16gb
+
+module load python2.7
+module load cuda/9.0
+source activate /home/cjuneau/.conda/envs/midi-env
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: ./training.sh <path-to-midi-files-dir>"
@@ -27,16 +31,5 @@ mkdir output/$OUTPUT_DIR
 
 THEANO_FLAGS="device=cuda,floatX=float32" python training_script.py $INPUT_DIR $OUTPUT_DIR 
 
-
-  
-
-
-# module load python2.7
-# module load cuda/9.0
-# source activate /home/cjuneau/.conda/envs/midi-env
-
-# cd "/work/cjuneau/biaxial-rnn-music-composition"
-# pwd
-# THEANO_FLAGS="device=cuda,floatX=float32" python training_script.py
-# conda deactivate 
-# python -c "print('done')"
+conda deactivate 
+python -c "print('done')"
--- training_script.py
+++ training_script.py
@@ -1,39 +1,35 @@
 # Carol Juneau
 
 import sys 
-input_dir = sys.argv[0]
-output_dir = sys.argv[1]
+import model
+import main
+import multi_training
+import cPickle as pickle
+import theano
+import time
 
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
 print ("input dir: " + input_dir)
 print("output dir: " + output_dir)
 
+startTime = time.time()
+print("using device: " + theano.config.device)
 
+pcs = multi_training.loadPieces(input_dir)
+print("finished loading pieces")
 
+m = model.Model([300,300],[100,50],dropout=0.5)
+print("constructed m")
 
-# import model
-# import main
-# import multi_training
-# import cPickle as pickle
-# import theano
-# import time
+multi_training.trainPiece(m,pcs,10001)
+print("trained m")
 
-# startTime = time.time()
-# print("using device: " + theano.config.device)
+pickle.dump(m.learned_config, open((output_dir+'/params_final.p'), 'wb'))
+print("saved model weights")
 
-# pcs = multi_training.loadPieces("music/gulfcoastcountry") #TODO variable for dataset name
-# print("finished loading pieces")
+# main.gen_adaptive(m, pcs, 1, name="composition") # TODO this??
+# print("composed piece")
 
-# m = model.Model([300,300],[100,50],dropout=0.5)
-# print("constructed m")
-
-# multi_training.trainPiece(m,pcs,10001)
-# print("trained m")
-
-# pickle.dump(m.learned_config, open('output/params_final.p', 'wb')) # TODO path
-# print("saved model weights")
-
-# # main.gen_adaptive(m, pcs, 1, name="composition") # TODO this??
-# # print("composed piece")
-
-# executionTime = (time.time() - startTime)
-# print("Total training time: " + time.strftime("%Hh %Mm %Ss", time.gmtime(executionTime)))
+executionTime = (time.time() - startTime)
+print("Total training time: " + time.strftime("%Hh %Mm %Ss", time.gmtime(executionTime)))
